6df17b727c46462e1dd2920c33029ee2b71dfc19
Maksim <106922768+Kalininmladshiy@users.noreply.github.com>
2023-03-25 15:37:18 +0300

    Added for if many files in folder

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,7 +1,6 @@
 import os
 import random
 import re
-from pathlib import Path
 
 
 def get_random_question(questions_and_answers):
@@ -22,14 +21,15 @@ def get_questions_answers(path_to_files):
     files_names = get_files_names(path_to_files)
     questions = []
     answers = []
-    with open(os.path.join(path_to_files, files_names[-1]), "r", encoding="KOI8-R") as my_file:
-        file_contents = my_file.read().split('\n\n')
-    for string in file_contents:
-        if 'Вопрос' in string:
-            questions.append(' '.join(string.splitlines()))
-        if 'Ответ:' in string:
-            answer = ' '.join(string.splitlines())[6:].lower().translate(remove_chars).strip()
-            answers.append(re.sub(r'\[.*\]', '', answer))
+    for file_name in files_names:
+        with open(os.path.join(path_to_files, file_name), "r", encoding="KOI8-R") as my_file:
+            file_contents = my_file.read().split('\n\n')
+        for string in file_contents:
+            if 'Вопрос' in string:
+                questions.append(' '.join(string.splitlines()))
+            if 'Ответ:' in string:
+                answer = ' '.join(string.splitlines())[6:].lower().translate(remove_chars).strip()
+                answers.append(re.sub(r'\[.*\]', '', answer))
 
     questions_and_answers = dict(zip(questions, answers))
     return questions_and_answers
